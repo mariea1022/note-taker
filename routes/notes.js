158e2440874218c1f96c6c4aa9c7dc40d713440c
Marie Lewis <mariea1022@gmail.com>
2022-12-11 18:47:20 -0800

    added uuid.js to generate id for each note added; added id to POST route in routes/notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 // using router method of express?
 const ns = require('express').Router();
+const uuid = require('../helpers/uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET route for retrieving all the notes
@@ -10,13 +11,14 @@ ns.get('/', (req, res) =>
 // POST route for submitting notes
 ns.post('/', (req, res) => {
     // destructuring assignment for items in re.body
-    const { title, text } = req.body;
+    const { title, text} = req.body;
     // if all the required properties are present
     if (title && text) {
         // variable for the object will save
         const newNote = {
             title,
-            text
+            text,
+            id: uuid(),
         }
         readAndAppend(newNote, './db/db.json');
 
