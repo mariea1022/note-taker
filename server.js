18944b887cc78939186cd00cd15f8ba9e85b9120
Marie Lewis <mariea1022@gmail.com>
2022-12-13 16:28:06 -0800

    added PORT for Heroku and start script for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { middleware } = require('./middleware/middleware');
 const api = require('./routes/index');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
